7e8daa48bc241d1cea9e1946cdcd173b6f3db42b
tranquil_N <993274966@qq.com>
2023-04-07 17:46:09 +0800

    netty

---
+++ src/main/java/com/tranquil/netty/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.tranquil.netty.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: tranquil_chen
+ * @Time: 2023/4/7  17:38
+ * @description:
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    public String test(){
+        return "hello word!";
+    }
+}
